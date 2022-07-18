97b33bf05ce0e1cd90594516114559cc9cce91ca
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-18 17:45:25 +0700

    fix: Модель user

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,21 +7,18 @@ const Unauthorized = require('../constants/Unauthorized');
 const userSchema = new mongoose.Schema({
   name: { // у пользователя есть имя — опишем требования к имени в схеме:
     type: String, // имя — это строка
-    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
     default: 'Your Name?',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'tell us about yourself',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: (value) => regExpLink.test(value),
     },
@@ -38,7 +35,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 3,
     select: false,
   },
 });
